c3fd302992d5c076f9db34ce6121dcf99f742472
Ann <ann.mushka@gmail.com>
2023-01-19 20:08:32 +0200

    change

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainerEl = document.querySelector('.gallery');
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" " />
+          </a>
+    `;
+    })
+    .join('');
+}
+
+galleryContainerEl.insertAdjacentHTML(
+  'beforeend',
+  createGalleryItemsMarkup(galleryItems)
+);
+
+galleryContainerEl.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
